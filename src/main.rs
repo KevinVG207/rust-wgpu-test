3c1b23fbf87b065f51375d6bb437eb0cf3c7f2dd
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-04-22 19:22:56 +0200

    Point and line primitives

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,40 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window
-};
-use std::error::Error;
+mod common;
+
+use winit::event_loop::EventLoop;
 use std::borrow::Cow;
-
-fn ensure_size_nonzero(size: winit::dpi::PhysicalSize<u32>) -> winit::dpi::PhysicalSize<u32> {
-    let new_width = size.width.max(1);
-    let new_height = size.height.max(1);
-    winit::dpi::PhysicalSize::new(new_width, new_height)
-}
-
-pub async fn run(event_loop: EventLoop<()>, window: Window) -> Result<(), Box<dyn Error>>{
-    let size = ensure_size_nonzero(window.inner_size());
-
-    let instance = wgpu::Instance::default();
-
-    let surface = instance.create_surface(&window)?;
-
-    let adapter = instance.request_adapter(
-        &wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            compatible_surface: Some(&surface),
-            force_fallback_adapter: false,
-        }
-    ).await.ok_or("No adapter found")?;
-
-    let (device, queue) = adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            label: None,
-            required_features: wgpu::Features::empty(),
-            required_limits: wgpu::Limits::default()
-        },
-        None
-    ).await?;
-
-    let mut config = surface
-        .get_default_config(&adapter, size.width, size.height)
-        .ok_or("Could not create surface config.")?;
-
-    surface.configure(&device, &config);
-
-    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-    });
-
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[],
-        push_constant_ranges: &[],
-    });
-
-    let swapchain_capabilities = surface.get_capabilities(&adapter);
-    let swapchain_format = swapchain_capabilities.formats[0];
-
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(swapchain_format.into())]
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
-
-    event_loop.run(move |event, window_target| {
-        window_target.set_control_flow(ControlFlow::Wait);
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                window_target.exit();
-            },
-
-            Event::WindowEvent {
-                event: WindowEvent::Resized(size),
-                ..
-            } => {
-                let size = ensure_size_nonzero(size);
-                config.width = size.width;
-                config.height = size.height;
-                surface.configure(&device, &config);
-            }
-
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                let frame = surface.get_current_texture().unwrap();
-                let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-                {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color {r: 0.05, g: 0.06, b: 0.08, a: 1.0}),
-                                store: wgpu::StoreOp::Store,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                        timestamp_writes: None,
-                        occlusion_query_set: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
-                }
-
-                queue.submit(Some(encoder.finish()));
-                frame.present();
-            }
-
-            _ => ()
-        }
-    })?;
-
-    Ok(())
-}
-
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut primitive_type = "point-list";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        primitive_type = &args[1];
+    }
+
+    let mut topology = wgpu::PrimitiveTopology::PointList;
+    let mut index_format = None;
+
+    if primitive_type == "line-list" {
+        topology = wgpu::PrimitiveTopology::LineList;
+        index_format = None;
+    } else if primitive_type == "line-strip" {
+        topology = wgpu::PrimitiveTopology::LineStrip;
+        index_format = Some(wgpu::IndexFormat::Uint32);
+    }
+
+    let inputs = common::Inputs{
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        topology,
+        strip_index_format: index_format,
+    };
+
     let event_loop = EventLoop::new()?;
-    let window = Window::new(&event_loop)?;
-    window.set_title("Hello, Triangle!");
+    let window = winit::window::Window::new(&event_loop)?;
+    window.set_title(&*format!("Hello, {}!", primitive_type));
+
     env_logger::init();
-    pollster::block_on(run(event_loop, window))?;
+
+    pollster::block_on(common::run(event_loop, window, inputs, 6))?;
 
     Ok(())
 }
