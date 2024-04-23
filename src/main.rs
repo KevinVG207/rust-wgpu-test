ccc7c243010da9d4d52bd547cc7a928194058fd6
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-04-23 11:55:39 +0200

    Triangles.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,31 @@ use std::borrow::Cow;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut primitive_type = "point-list";
+    let mut primitive_type = "triangle-list";
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         primitive_type = &args[1];
     }
 
-    let mut topology = wgpu::PrimitiveTopology::PointList;
+    let mut topology = wgpu::PrimitiveTopology::TriangleList;
     let mut index_format = None;
 
-    if primitive_type == "line-list" {
+    // let mut topology: wgpu::PrimitiveTopology = wgpu::PrimitiveTopology::PointList;
+    // let mut index_format: Option<wgpu::IndexFormat> = None;
+
+
+    // if primitive_type == "triangle-list" {
+    //     topology = wgpu::PrimitiveTopology::TriangleList;
+    //     index_format = Some(wgpu::IndexFormat::Uint32);
+    // }
+    if primitive_type == "triangle-strip" {
+        topology = wgpu::PrimitiveTopology::TriangleStrip;
+        index_format = Some(wgpu::IndexFormat::Uint32);
+    }
+    else if primitive_type == "point-list" {
+        topology = wgpu::PrimitiveTopology::PointList;
+        index_format = None;
+    } else if primitive_type == "line-list" {
         topology = wgpu::PrimitiveTopology::LineList;
         index_format = None;
     } else if primitive_type == "line-strip" {
@@ -34,7 +49,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     env_logger::init();
 
-    pollster::block_on(common::run(event_loop, window, inputs, 6))?;
+    pollster::block_on(common::run(event_loop, window, inputs, 9))?;
 
     Ok(())
 }
